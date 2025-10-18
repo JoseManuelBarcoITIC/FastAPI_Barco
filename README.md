c9fef7ec3dd4041df5bc9d7c33ce33ed2f993b74
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2025-10-18 15:35:48 +0200

    Añadio las images al README i ademas corregido un endpoint

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # FastAPI_Barco
+
+# Activitat-RA3---PYTHON-
+![Ejercicio 1](images/ejercicio1.png)
+
+![Ejercicio 2](images/ejercicio2.png)
+
+![Ejercicio 3](images/ejercicio3.png)
+
+![Ejercicio 4](images/ejercicio4.png)
+
+![Ejercicio 5](images/ejercicio5.png)
+
--- main.py
+++ main.py
@@ -12,29 +12,18 @@ async def add_user( ):
     return user_dict
 
 @app.get("/api/users/{id}", response_model = dict)
-async def get_user(user_id: int):
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
+async def get_user(id: int):
+    user_id = range(len(usersname))
+    user_dict = dict(zip(user_id, usersname))
 
-    if user_id in user_dict:
-        return{
-            "id": user_id,
-            "name": user_dict[user_id]
-        }
+    if id in user_dict:
+        return {id: user_dict[id]}
 
 @app.get("/api/users", response_model = dict)
 async def get_user_list():
     id = range(len(usersname))
     user_dict = dict(zip(id, usersname))
     return user_dict
-
-@app.put("/api/users", response_model = dict)
-def modify_user(user_id: int, new_name: str):
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
-    if user_id in user_dict:
-        user_dict[user_id] = new_name
-        return user_dict
 
 @app.put("/api/users", response_model = dict)
 def modify_user(user_id: int, new_name: str):
