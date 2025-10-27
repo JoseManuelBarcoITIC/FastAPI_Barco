dc9bde9096c523f00892826d98bcd5f65feb3442
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2025-10-27 19:32:19 +0100

    Echo 7,8 i 9

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI,Depends
-from sqlalchemy import create_engine
-from sqlmodel import SQLModel, Field, Session, select, delete
-
-from models.User import User
+from sqlalchemy import create_engine, update
+from sqlmodel import SQLModel, Session, select, delete
 from models.Product import Product, ProductRequest,ProductResponse, ProductResponsePartial
 
 import os
@@ -29,16 +27,19 @@ def add_user(product: ProductRequest,db:Session = Depends(get_db)):
     db.add(insert_product)
     db.commit()
     return {"msg":"Afegit usuari correctament"}
+
 @app.get("/product/{id}", response_model=ProductResponse, tags = ["GETS"])
 def Get_Product_By_Id(id:int ,db:Session = Depends(get_db)):
     stmt = select(Product).where(Product.id == id)
     result = db.exec(stmt).first()
     return ProductResponse.model_validate(result)
+
 @app.get("/products", response_model=list[ProductRequest], tags = ["GETS"])
 def Get_Product_List(db:Session = Depends(get_db)):
     stmt = select(Product)
     result = db.exec(stmt).all()
     return result
+
 @app.get("/products/{price}", response_model=list[ProductRequest], tags = ["GETS"])
 def Get_All_Products_List_By_Price(price:int,db:Session = Depends(get_db)):
     stmt = select(Product).where(Product.price == price)
@@ -52,6 +53,40 @@ def Get_All_Products_Partial(db:Session = Depends(get_db)):
 @app.delete("/products/{id}", response_model=dict, tags = ["DELETES"])
 def Delete_Product(id:int,db:Session = Depends(get_db)):
     stmt = delete(Product).where(Product.id == id)
-    result = db.exec(stmt)
+    db.exec(stmt)
     db.commit()
     return {"msg":"Usuari eliminat correctament"}
+
+@app.put("/products/put/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Product(id:int, product:ProductRequest,db:Session = Depends(get_db)):
+    update_product = product.model_dump()
+    stmt = (
+        update(Product)
+        .where(Product.id == id)
+        .values(update_product)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Usuari modificat correctament"}
+
+@app.patch("/products/patch/price/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Price_Product(id:int,price: int,db:Session = Depends(get_db)):
+    stmt = (
+        update(Product)
+        .where(Product.id == id)
+        .values(price = price)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Usuari modificat correctament"}
+
+@app.patch("/products/patch/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Partial_Product(product_id:int,price: int,name:str,db:Session = Depends(get_db)):
+    stmt = (
+        update(Product)
+        .where(Product.id == product_id)
+        .values(price = price, name = name)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Usuari modificat correctament"}
