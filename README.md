8ee184e8856057c66c7db6def1d2f4789f2dc968
JoseManuelBarcoITIC <2526_jose.barco@iticbcn.cat>
2025-11-01 09:41:03 +0100

    Ejercicio acabado

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # FastAPI_Barco
 
 # Activitat-RA3---PYTHON-
-![Ejercicio 1](images/ejercicio1.png)
+![Ejercicio 1 Peticion](images/Ejercicio1_Peticion.png)
 
-![Ejercicio 2](images/ejercicio2.png)
+![Ejercicio 1 Respuesta](images/Ejercicio1_Respuesta.png)
 
-![Ejercicio 3](images/ejercicio3.png)
+![Ejercicio 2](images/Ejercicio2.png)
 
-![Ejercicio 4](images/ejercicio4.png)
+![Ejercicio 3](images/Ejercicio3.png)
 
-![Ejercicio 5](images/ejercicio5.png)
+![Ejercicio 4](images/Ejercicio4.png)
+
+![Ejercicio 5](images/Ejercicio5.png)
+
+![Ejercicio 6](images/Ejercicio6.png)
+
+![Ejercicio 7 Peticion](images/Ejercicio7_Peticion.png)
+
+![Ejercicio 7 Respuesta](images/Ejercicio7_Respuesta.png)
+
+![Ejercicio 8](images/Ejercicio8.png)
+
+![Ejercicio 9 Peticion](images/Ejercicio9_Peticion.png)
+
+![Ejercicio 9 Respuesta](images/Ejercicio9_Repuesta.png)
+
+
 
--- main.py
+++ main.py
@@ -1,41 +1,92 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI,Depends
+from sqlalchemy import create_engine, update
+from sqlmodel import SQLModel, Session, select, delete
+from models.Product import Product, ProductRequest,ProductResponse, ProductResponsePartial
 
-from fastapi import FastAPI
+import os
 
-usersname = ["pepe","manolo"]
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
+
+def get_db():
+    db= Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
 app = FastAPI()
-@app.post("/api/users", response_model = dict)
-async def add_user( ):
-    new_user = "juani"
-    usersname.append(new_user)
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
-    return user_dict
 
-@app.get("/api/users/{id}", response_model = dict)
-async def get_user(id: int):
-    user_id = range(len(usersname))
-    user_dict = dict(zip(user_id, usersname))
+@app.post("/product", response_model = dict, tags = ["CREATE"])
+def add_user(product: ProductRequest,db:Session = Depends(get_db)):
+    insert_product = Product.model_validate(product)
+    db.add(insert_product)
+    db.commit()
+    return {"msg":"Afegit producte correctament"}
 
-    if id in user_dict:
-        return {id: user_dict[id]}
+@app.get("/product/{id}", response_model=ProductResponse, tags = ["GETS"])
+def Get_Product_By_Id(id:int ,db:Session = Depends(get_db)):
+    stmt = select(Product).where(Product.id == id)
+    result = db.exec(stmt).first()
+    return ProductResponse.model_validate(result)
 
-@app.get("/api/users", response_model = dict)
-async def get_user_list():
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
-    return user_dict
+@app.get("/products", response_model=list[ProductRequest], tags = ["GETS"])
+def Get_Product_List(db:Session = Depends(get_db)):
+    stmt = select(Product)
+    result = db.exec(stmt).all()
+    return result
 
-@app.put("/api/users", response_model = dict)
-def modify_user(user_id: int, new_name: str):
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
-    if user_id in user_dict:
-        user_dict[user_id] = new_name
-        return user_dict
-@app.delete("/api/users", response_model = dict)
-def delete_user(user_id: int):
-    id = range(len(usersname))
-    user_dict = dict(zip(id, usersname))
-    if user_id in user_dict:
-        user_dict.pop(user_id)
-        return user_dict
+@app.get("/products/{price}", response_model=list[ProductRequest], tags = ["GETS"])
+def Get_All_Products_List_By_Price(price:int,db:Session = Depends(get_db)):
+    stmt = select(Product).where(Product.price == price)
+    result = db.exec(stmt).all()
+    return result
+@app.get("/productspartial", response_model=list[ProductResponsePartial], tags = ["GETS"])
+def Get_All_Products_Partial(db:Session = Depends(get_db)):
+    stmt = select(Product)
+    result = db.exec(stmt).all()
+    return result
+@app.delete("/products/{id}", response_model=dict, tags = ["DELETES"])
+def Delete_Product(id:int,db:Session = Depends(get_db)):
+    stmt = delete(Product).where(Product.id == id)
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Producte eliminat correctament"}
+
+@app.put("/products/put/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Product(id:int, product:ProductRequest,db:Session = Depends(get_db)):
+    update_product = product.model_dump()
+    stmt = (
+        update(Product)
+        .where(Product.id == id)
+        .values(update_product)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Producte modificat correctament"}
+
+@app.patch("/products/patch/price/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Price_Product(id:int,price: int,db:Session = Depends(get_db)):
+    stmt = (
+        update(Product)
+        .where(Product.id == id)
+        .values(price = price)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Preu del producte modificat correctament"}
+
+@app.patch("/products/patch/{id}", response_model=dict, tags = ["UPDATES"])
+def Update_Partial_Product(product_id:int,price: int,name:str,db:Session = Depends(get_db)):
+    stmt = (
+        update(Product)
+        .where(Product.id == product_id)
+        .values(price = price, name = name)
+    )
+    db.exec(stmt)
+    db.commit()
+    return {"msg":"Nom i preu del producte modificat correctament"}
